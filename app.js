c07b91abbc6c27feafdaace146550c64d4a14a78
jethroo <cwirth79@web.de>
2014-07-12 22:49:39 +0200

    fixing delete from cart

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ angular.module('tutorialApp', ['ngAnimate'])
         items.push(article);
       },
       deleteArticle: function(article) {
-        items.pop(article);
+        index = items.indexOf(article);
+        if (index > -1)
+          items.splice(index, 1);
+        end
       },
       sum: function() {
         return items.reduce(function(total, article) {
