244aad63e85d0393695859e3b42fa135f38f9c3c
jethroo <cwirth79@web.de>
2014-07-12 22:11:51 +0200

    stepped to ngAnimate

---
+++ app.js
@@ -0,0 +1,29 @@
+angular.module('tutorialApp', ['ngAnimate'])
+  .factory('Cart', function() {
+    var items = [];
+    return {
+      getItems: function() {
+        return items;
+      },
+      addArticle: function(article) {
+        items.push(article);
+      },
+      deleteArticle: function(article) {
+        items.pop(article);
+      },
+      sum: function() {
+        return items.reduce(function(total, article) {
+          return total + article.price;
+        }, 0);
+      }
+    };
+  })
+  .controller('ArticlesCtrl', function($scope, $http, Cart){
+    $scope.cart = Cart;
+    $http.get('articles.json').then(function(articlesResponse) {
+      $scope.articles = articlesResponse.data;
+    });
+  })
+  .controller('CartCtrl', function($scope, Cart){
+    $scope.cart = Cart;
+  });
