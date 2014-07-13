8208771f75e2466852457bf41b2d33aad2ab59e0
jethroo <cwirth79@web.de>
2014-07-13 18:16:43 +0200

    starting minimization with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var ngAnnotate = require('gulp-ng-annotate')
 
 gulp.task('js', function () {
   gulp.src(['src/**/module.js', 'src/**/*.js'])
   .pipe(concat('app.js'))
+  .pipe(uglify())
+  .pipe(ngAnnotate())
   .pipe(gulp.dest('.'))
 })
 
